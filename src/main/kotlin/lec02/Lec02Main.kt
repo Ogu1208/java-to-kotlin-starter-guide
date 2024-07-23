92c50b16a4015855b0bba60adcd7c330b5b05377
Ogu1208 <kdasunb6@gmail.com>
2024-07-24 01:01:23 +0900

    ch03: 2. safe call, elvis 를 사용한 코틀린 방식의 null 처리

--- src/main/kotlin/lec02/Lec02Main.kt
+++ src/main/kotlin/lec02/Lec02Main.kt
@@ -1,28 +1,34 @@
 package lec02
 
 fun main() {
-
+    println(startsWith5("ABC"))  // true
+    println(startsWith5("BC"))  // false
+    println(startsWith5(null))  // Runtime에서 NullPointerException 발생
 }
 
 fun startsWithA1(str: String?): Boolean {  // java boolean -> null이 들어올 수 없음
-    if (str == null) {
-        throw IllegalArgumentException("null이 들어왔습니다")
-    }
-    return str.startsWith("A")
+    return str?.startsWith("A")
+        ?: throw IllegalArgumentException("str is null")
+    // 앞이 null일 경우 전체가 null이 되면서 ?: 뒤의 Exception 발생
 }
 
 fun startsWithA2(str: String?): Boolean? {  // java Boolean -> null이 들어올 수 있음
-    if (str == null) {
-        return null
-    }
-    return str.startsWith("A")
+    return str?.startsWith("A")
+    // Safe Call은 앞에 변수가 null 그대로 null이 된다.
+    // 앞 (str)이 null일 경우 전체가 null이 되면서 그대로 null 반환
 }
 
 fun startsWithA3(str: String?): Boolean {
-    if (str == null) {
-        return false
-    }
+    return str?.startsWith("A") ?: false
+    // 앞(str)이 null일 경우 전체가 null이 되면서 ?: 뒤의 false 반환
+
+}
+
+fun startsWithA4(str: String): Boolean {  // String, Boolean 모두 Null X
     return str.startsWith("A")
-    // 코틀린에서는 null일 수 있는 변수에서 바로 method call 불가능
-    // 그러나 문맥 상 위에서 null을 체크하면 이후에 null이 아닐것이라고 컴파일러가 추측
+}
+
+fun startsWith5(str: String?): Boolean {
+    return str!!.startsWith("A")
+    // !! : null이 아님을 보장하는 연산자
 }
